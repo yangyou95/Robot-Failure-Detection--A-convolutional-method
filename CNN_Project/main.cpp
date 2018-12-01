9e4b8e8f2e88212ddbe6033f98a07650623b0e75
Yang <bboylevi89@gmail.com>
2018-12-01 01:27:05 +0000

    Initial Commit

---
+++ CNN_Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CNN_Project
+//
+//  Created by Yang on 2018/12/1.
+//  Copyright © 2018 Yang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
